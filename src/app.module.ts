8c1719dbc72dad8041bb5564a225623d33e2fd24
Michael <tngmichael89@gmail.com>
2020-03-07 16:15:24 +0700

    feat: dynamic import based on plugins.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { DatabaseModule } from './database/database.module';
 import { PeopleModule } from './people/people.module';
-import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
@@ -11,7 +10,6 @@ import { PostsModule } from './posts/posts.module';
     }),
     DatabaseModule,
     PeopleModule,
-    PostsModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import * as jsonfile from 'jsonfile';
 import { AppModule } from './app.module';
 
+const FILE = 'plugins.json';
+
+async function resolveImport(name) {
+  return new Promise(resolve => resolve(import(`./${name}/${name}.module.js`)));
+}
+
+async function getPlugins() {
+  const plugins = [];
+  const fileData = jsonfile.readFileSync(FILE);
+
+  for (const pluginDetail of fileData.plugins) {
+    const plugin = await resolveImport(pluginDetail.path);
+    plugins.push(plugin[pluginDetail.name]);
+  }
+
+  return new Promise(resolve => resolve(plugins));
+}
+
 async function bootstrap() {
+  const plugins = await getPlugins();
+  const currentModules = Reflect.getOwnMetadata('imports', AppModule);
+  Reflect.defineMetadata('imports', currentModules.concat(plugins), AppModule);
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
