bdf12cbff042062b3b14b3065662b17d6a46b213
Michael <tngmichael89@gmail.com>
2020-03-07 10:53:13 +0700

    chore: update people seed

--- src/database/seeds/01-People.ts
+++ src/database/seeds/01-People.ts
@@ -1,4 +1,19 @@
 import * as Knex from 'knex';
+import { PersonModel } from '../models/person.model';
 
 export async function seed(knex: Knex): Promise<any> {
+  const data = [
+    {
+      id: 1,
+      name: 'John Doe',
+    },
+    {
+      id: 2,
+      name: 'Mary Doe',
+    },
+  ];
+  await PersonModel.query(knex).delete();
+  for (let i = 0; i < data.length; i += 1) {
+    await PersonModel.query(knex).insert(data[i]);
+  }
 }
