f6f534e05360b5f9e63c429f83180d1c2099a735
Michael <tngmichael89@gmail.com>
2020-03-07 11:42:41 +0700

    chore: add mock

--- src/database/seeds/01-People.ts
+++ src/database/seeds/01-People.ts
@@ -1,19 +1,12 @@
 import * as Knex from 'knex';
 import { PersonModel } from '../models/person.model';
+import { mockData } from '../mock';
+
+const { peopleMock } = mockData;
 
 export async function seed(knex: Knex): Promise<any> {
-  const data = [
-    {
-      id: 1,
-      name: 'John Doe',
-    },
-    {
-      id: 2,
-      name: 'Mary Doe',
-    },
-  ];
   await PersonModel.query(knex).delete();
-  for (let i = 0; i < data.length; i += 1) {
-    await PersonModel.query(knex).insert(data[i]);
+  for (let i = 0; i < peopleMock.length; i += 1) {
+    await PersonModel.query(knex).insert(peopleMock[i]);
   }
 }
